01df59f152cdffd2fd28b281f5347ad3e9721563
Clark Vincent D. Enriquez <vince.cody@gmail.com>
2024-02-05 16:08:09 +0800

    convert forms to vue

--- apiCall_jquery.js
+++ apiCall_jquery.js
@@ -1,58 +1,85 @@
 $(document).ready(function(){
-    
+
+
+    var sec = 1000;
+    var min = sec * 60;
+    var iteration;
+
     $("#run-test").on('click', function(){
 
-        var user_ids= [];
+        iteration = 0;
+        user_number = $("#numUsers").val();
+ 
+        runTestInterval = setInterval(run_test, sec * 30);
+ 
+    }); 
+    
+    
+    function run_test(){
+ 
+            var user_ids= [];
+                        
+            if(iteration == 60){
 
-        for(let i= 1; i <= user_number; i++){
-            user_ids[i -1] = i;
-        }
+                clearInterval(runTestInterval, min * 30);
 
-        user_ids.forEach(function(user_id){
-
-                var url = stg_url + move_history_batch_url + user_id ;
+                console.log('stress test completed!');
                 
-                var ajaxTime;
+                alert('stress test complete! ');
+
+            }
+
+            for(let i= 1; i <= user_number; i++){
+                user_ids[i -1] = i;
+            }
+
+            user_ids.forEach(function(user_id){
+
+                    var url = stg_url + move_history_batch_url + user_id ;
+                    
+                    var ajaxTime;
 
 
-                $.ajax({
+                    $.ajax({
 
-                    type: "POST",
-                    url: url,
-                    async: true,
-                    //dataType: 'json',
-                    contentType: 'application/json',
-                    data: JSON.stringify(coordinatesJson),
-                    beforeSend: function(){
-                        ajaxTime= new Date().getTime();
-                    },
-                    complete: function (response, status) {
+                        type: "POST",
+                        url: url,
+                        async: true,
+                        //dataType: 'json',
+                        contentType: 'application/json',
+                        data: JSON.stringify(coordinatesJson),
+                        beforeSend: function(){
+                            ajaxTime= new Date().getTime();
+                        },
+                        complete: function (response, status) {
 
-                        var totalTime = new Date().getTime() - ajaxTime;
-                       // $(".logs-container table").append("<tr><td>" + user_id + "<td>" + totalTime + "ms <td>" + status);
-
+                            var totalTime = new Date().getTime() - ajaxTime;
                         // $(".logs-container table").append("<tr><td>" + user_id + "<td>" + totalTime + "ms <td>" + status);
 
-                        
-                    },  
-                    success: function (response, status) {
-                        
-                        var totalTime = new Date().getTime() - ajaxTime;
-                        
-                        $(".logs-container table").append("<tr><td>" + user_id + "<td>" + totalTime + "ms <td>" + status);
+                            // $(".logs-container table").append("<tr><td>" + user_id + "<td>" + totalTime + "ms <td>" + status);
 
-                    }
+                            
+                        },  
+                        success: function (response, status) {
+                            
+                            var totalTime = new Date().getTime() - ajaxTime;
+                            
+                            $(".logs-container table").append("<tr><td>" + user_id + "<td>" + totalTime + "ms <td>" + status);
 
-                });
+                        }
 
-        });
+                    });
 
-        console.log(coordinatesJson);
+            });
 
-        return;
+            iteration++;
 
-        
-    });
+            console.log(coordinatesJson);
+            
+            
+            return;
+
+        }
 
 
 });
